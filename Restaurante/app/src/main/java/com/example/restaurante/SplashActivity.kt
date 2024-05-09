9ab186fbf81c240745d13f7c310ba4f02d5ca80e
mariaritacst <166262923+mariaritacst@users.noreply.github.com>
2024-05-09 17:25:15 -0300

    Update SplashActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/SplashActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/SplashActivity.kt
@@ -7,14 +7,18 @@ import android.os.Handler
 import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+    //Define o layout da nossa página
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
+        //Cria a variável e atribui um valor a ela
             val i = intent
+        //Cria uma intent que provavelmente será usada para iniciar um pedido
             val j = Intent(this,PedidoActivity::class.java)
             j.putExtras(i)
+        //Inicia a atividade PedidoActivity
             startActivity(j)
         },2000)
 
